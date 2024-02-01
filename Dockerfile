6927ef283d76140fd9ed599b81d8f4569eeb01ed
Stanislav <stanhjrpower@gmail.com>
2024-02-01 23:03:20 +0200

    update cacher

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,16 @@
-# Используем официальный образ Golang
 FROM golang:latest
 
-# Устанавливаем рабочую директорию внутри контейнера
 WORKDIR /app
 
-# Копируем файлы go.mod и go.sum и устанавливаем зависимости
 COPY go.mod .
 COPY go.sum .
 RUN go mod download
 
-ARG REDIS_ADDR
-ARG REDIS_PASSWORD
-# Копируем файлы проекта в текущую директорию
 COPY . .
 
-# Собираем Go-приложение
 RUN go build -o main .
 
-# Указываем порт, который будет прослушивать приложение
 EXPOSE 8080
 
-# Команда для запуска приложения
 CMD ["./main"]
 
--- main.go
+++ main.go
@@ -1,36 +1,67 @@
 package main
 
 import (
+	"back_office_cacher/utils"
 	"fmt"
 	"github.com/go-redis/redis"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"io"
 	"net/http"
 	"net/url"
+	"os"
+	"time"
 )
 
 var redisClient *redis.Client
 
+var (
+	httpRequestsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests",
+		},
+		[]string{"method", "endpoint"},
+	)
+)
+
 func init() {
 	redisClient = redis.NewClient(&redis.Options{
-		Addr:     "redis:6379",
-		Password: "8QQnjhHdnAj9ZgVpB5AZSDpZCHpLne",
+		Addr: os.Getenv("REDIS_ADDR"),
 	})
+	prometheus.MustRegister(httpRequestsTotal)
 }
 
 func proxyHandler(w http.ResponseWriter, r *http.Request) {
+	httpRequestsTotal.WithLabelValues(r.Method, r.URL.Path).Inc()
 	partnerID := r.Header.Get("Partner-Id")
 	authorization := r.Header.Get("Authorization")
 
-	cacheKey := fmt.Sprintf("%s:%s", partnerID, authorization)
-	cachedResponse, err := redisClient.Get(cacheKey).Result()
-
-	if err == nil {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(cachedResponse))
+	if partnerID == "" || authorization == "" {
+		http.Error(w, "Authentication credentials were not provided.", http.StatusUnauthorized)
+		return
+	}
+	cacheService := utils.NewCacheService(partnerID, authorization, r.URL.Path)
+	cacheKey, err := cacheService.GetCacheKey()
+	if err != nil {
+		http.Error(w, "Resource not found.", http.StatusNotFound)
 		return
 	}
 
-	remoteURL := "https://backoffice-backend.dev.p13r.bcua.io"
+	cachedResponse, err := utils.RetrieveFromCache(redisClient, cacheKey)
+
+	if err == nil {
+		for key, values := range cachedResponse.Headers {
+			for _, value := range values {
+				w.Header().Add(key, value)
+			}
+		}
+		w.WriteHeader(cachedResponse.Status)
+		w.Write([]byte(cachedResponse.Body))
+		return
+	}
+
+	remoteURL := os.Getenv("PROXY_URL")
 	remote, err := url.Parse(remoteURL)
 	if err != nil {
 		http.Error(w, "Failed to parse remote URL", http.StatusInternalServerError)
@@ -42,6 +73,7 @@ func proxyHandler(w http.ResponseWriter, r *http.Request) {
 	r.Host = remote.Host
 
 	resp, err := http.DefaultTransport.RoundTrip(r)
+
 	if err != nil {
 		http.Error(w, "Failed to make request to remote server", http.StatusInternalServerError)
 		return
@@ -49,13 +81,23 @@ func proxyHandler(w http.ResponseWriter, r *http.Request) {
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
+
 	if err != nil {
 		http.Error(w, "Failed to read response body", http.StatusInternalServerError)
 		return
 	}
 
-	if resp.StatusCode == http.StatusOK {
-		err := redisClient.Set(cacheKey, string(body), 0).Err()
+	keyTtl := 30 * time.Minute
+
+	if resp.StatusCode < 500 {
+		err := utils.CacheResponse(
+			redisClient,
+			cacheKey,
+			resp,
+			string(body),
+			keyTtl,
+		)
+
 		if err != nil {
 			fmt.Println("Failed to cache response in Redis:", err)
 		}
@@ -71,6 +113,7 @@ func proxyHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	http.Handle("/metrics", promhttp.Handler())
 	http.HandleFunc("/", proxyHandler)
 
 	port := 8080
