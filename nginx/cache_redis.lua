80ef4147a319d55d3885004186432b97b6ca156e
Stanislav <stanhjrpower@gmail.com>
2024-02-04 10:17:32 +0200

    test conf

--- nginx/cache_redis.lua
+++ nginx/cache_redis.lua
@@ -6,7 +6,8 @@ red:set_timeout(1000) -- 1 second
 
 local options_table = {}
 options_table["pool"] = "docker_server"
-local ok, err = red:connect("redis", 6379, options_table)
+red:connect("redis", 6379, options_table)
+
 local value, err = red:get(cacheKey)
 
 if value == ngx.null then
